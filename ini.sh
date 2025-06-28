81243cb82001e4a68b3a5f644b8e47bb4a3cd817
smarterThings <102267679+epulaecorp@users.noreply.github.com>
2025-06-28 12:08:14 -0700

    Update ini.sh

--- ini.sh
+++ ini.sh
@@ -1,1 +1,11 @@
-wget -O - https://raw.githubusercontent.com/epulaecorp/NAS/main/setup.sh | bash
+# Define la URL base para no repetirla
+BASE_URL="https://raw.githubusercontent.com/epulaecorp/NAS/main"
+
+echo "Iniciando descarga y sobrescritura de archivos..."
+
+# Descarga cada archivo, forzando la sobrescritura con la opción -O
+wget -O docker-compose.yml "${BASE_URL}/docker-compose.yml"
+wget -O docker-updater.sh "${BASE_URL}/docker-updater.sh"
+wget -O setup.sh "${BASE_URL}/setup.sh"
+
+echo "Archivos actualizados correctamente."
