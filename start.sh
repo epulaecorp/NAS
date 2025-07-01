e5d51c0370b41edd6e6100952dbc502371bde48a
smarterThings <102267679+epulaecorp@users.noreply.github.com>
2025-06-30 21:22:34 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
+sudo rm -f /volume1/docker/*.sh /volume1/docker/docker-compose.yml
+
 sudo mkdir -p /volume1/docker && cd /volume1/docker && sudo wget --no-check-certificate -q https://raw.githubusercontent.com/epulaecorp/NAS/main/{docker-compose.yml,setup.sh,updater.sh} && sudo chmod +x *.sh
